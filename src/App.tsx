f38696c17076aba3b74a0c681c46f9eba0dbcce4
draytchev <draytchev@gmail.com>
2026-05-18 11:06:13 +0300

    added laodeer when changing apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import LogViewer from "./components/LogViewer/LogViewer";
+import { useLogs } from "./hooks/useLogs";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const { loading } = useLogs();
+
   return (
     <div className="app">
+      {loading && (
+        <div className="loader-overlay">
+          <Loader size={20} />
+        </div>
+      )}
+
       <Header />
       <LogViewer />
     </div>
